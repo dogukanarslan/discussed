2d31a2e7824f1e9de153c5fb72fe5a440f96d70a
Dogukan Arslan <dogukan2arslan@hotmail.com>
2025-11-18 00:02:30 +0300

    Return error json

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ export const signin = (req, res) => {
       res.status(200).json({ id: user.id, username: user.username });
     }
   } else {
-    res.status(404).text("User not found");
+    res.status(404).json({error: "User not found"});
   }
 };
 
