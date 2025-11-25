bf69dc93f48bb494a016536ce75e538221ecf4de
Dogukan Arslan <dogukan2arslan@hotmail.com>
2025-11-25 22:30:21 +0000

    Delete unused method

--- db/db.js
+++ db/db.js
@@ -1,17 +1,13 @@
-import {DatabaseSync} from 'node:sqlite';
-import path from 'node:path';
-import fs from 'node:fs';
-import {fileURLToPath} from 'node:url';
+import { DatabaseSync } from "node:sqlite";
+import path from "node:path";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
 
 export const database = new DatabaseSync(process.env.DB);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const schemaPath = path.join(__dirname, 'schema.sql');
-const schema = fs.readFileSync(schemaPath, 'utf-8');
+const schemaPath = path.join(__dirname, "schema.sql");
+const schema = fs.readFileSync(schemaPath, "utf-8");
 database.exec(schema);
-
-export const createMessage = database.prepare(
-  'INSERT INTO messages (user_id, message) VALUES (?, ?)'
-);
