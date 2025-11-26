e00279e73227d463c5b946e6d2b6678708345dc2
Dogukan Arslan <dogukan2arslan@hotmail.com>
2025-11-26 21:42:16 +0000

    Use db in home directory

--- db/db.js
+++ db/db.js
@@ -3,7 +3,9 @@ import path from "node:path";
 import fs from "node:fs";
 import { fileURLToPath } from "node:url";
 
-export const database = new DatabaseSync(process.env.DB);
+export const database = new DatabaseSync(
+  `${process.env.HOME}/${process.env.DB}`
+);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
