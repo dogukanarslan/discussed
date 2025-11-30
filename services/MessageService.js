1bac3ca8bffbe0b3b57c146aee590243c7927246
Dogukan Arslan <dogukan2arslan@hotmail.com>
2025-11-30 10:11:55 +0000

    Fix message service errors

--- services/MessageService.js
+++ services/MessageService.js
@@ -7,19 +7,20 @@ export const MessageService = {
   getById(msgId) {
     const msg = MessageModel.get(msgId);
     if (!msg) {
-      throw Error("Message not found");
+      throw { status: 404, message: "Message not found" };
     }
 
     return msg;
   },
   create({ user_id, message }) {
     if (!user_id) {
-      return res.status(400).json({ error: "user_id is required" });
+      throw { status: 400, message: "user_id is required" };
     }
 
     if (!message) {
-      return res.status(400).json({ error: "message is required" });
+      throw { status: 400, message: "message is required" };
     }
+
     const { id } = MessageModel.create(user_id, message);
     return MessageModel.get(id);
   },
