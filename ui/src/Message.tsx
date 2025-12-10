3afc1426fe38da40fe9e097a5e24319dbffcfec7
Dogukan Arslan <dogukan2arslan@hotmail.com>
2025-12-10 22:12:08 +0000

    Show joined user

--- ui/src/Message.tsx
+++ ui/src/Message.tsx
@@ -1,7 +1,12 @@
 import { formatTime } from "./constants";
 
 interface Props {
-  message: { username: string; message: string; created_at: string };
+  message: {
+    username: string;
+    message: string;
+    created_at: string;
+    new_user: string;
+  };
   username: string;
 }
 
@@ -9,6 +14,12 @@ export const Message = (props: Props) => {
   const { message, username } = props;
 
   const isCurrentUser = username === message.username;
+
+  if (message.new_user) {
+    return (
+      <div> {message.new_user} joined</div>
+    );
+  }
 
   return (
     <div className="message">
