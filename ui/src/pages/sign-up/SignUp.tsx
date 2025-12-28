18860a25718eb39154d0c03baf2d8dbab715fa47
Dogukan Arslan <dogukan2arslan@hotmail.com>
2025-12-28 19:33:54 +0300

    Redirect to chat on sign up

--- ui/src/pages/sign-up/SignUp.tsx
+++ ui/src/pages/sign-up/SignUp.tsx
@@ -31,7 +31,7 @@ export const SignUp = (props: Props) => {
       .then((data) => {
         sessionStorage.setItem('user', JSON.stringify(data));
         setUser({username: data.username});
-        window.location.hash = '#signin';
+        window.location.hash = '#chat';
       })
       .catch((e) => {
         setError(e.message);
