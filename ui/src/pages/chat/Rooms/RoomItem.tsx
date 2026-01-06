8f17353917b1a567d554d6356b95b64e70d1f96e
Dogukan Arslan <dogukan2arslan@hotmail.com>
2026-01-07 01:14:13 +0300

    Prevent event propagation on delete

--- ui/src/pages/chat/Rooms/RoomItem.tsx
+++ ui/src/pages/chat/Rooms/RoomItem.tsx
@@ -10,13 +10,18 @@ interface Props {
 export const RoomItem = (props: Props) => {
   const { name, selected, handleDelete, changeRoom } = props;
 
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    handleDelete();
+  };
+
   return (
     <div
       className={`room-item ${selected ? 'room-item__selected' : ''}`}
       onClick={changeRoom}
     >
       <div className="room-item__name">{name}</div>
-      <button className="room-item__delete-btn" onClick={handleDelete}>
+      <button className="room-item__delete-btn" onClick={handleClick}>
         {`\u2613`}
       </button>
     </div>
