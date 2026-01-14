6ce86b362ba47c61d870126f11b79f0673940360
Dogukan Arslan <dogukan2arslan@hotmail.com>
2026-01-15 00:22:40 +0300

    Rename file extension

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signin, signup } from '../controllers/authController.js';
+import { signin, signup } from '../controllers/authController.ts';
 
 export const router = express.Router();
 
