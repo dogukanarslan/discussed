f06df3888d40de65e7f858cd0b2d75dee7d22a4c
Dogukan Arslan <dogukan2arslan@hotmail.com>
2026-01-23 01:16:07 +0300

    Split if cookies exist

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const validateToken = (req, res, next) => {
   const cookies = req.headers.cookie;
-  const token = cookies.split('=')[1];
+  const token = cookies?.split('=')[1];
   try {
     jwt.verify(token, process.env.JWT_SECRET);
     next();
