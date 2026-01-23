e6b85e2f4965c3d6b88b2a6fd844f15c68b7d13d
Dogukan Arslan <dogukan2arslan@hotmail.com>
2026-01-24 01:55:13 +0300

    Move import to the top

--- controllers/messageController.ts
+++ controllers/messageController.ts
@@ -1,5 +1,5 @@
-import { MessageService } from '../services/MessageService.js';
 import type { Request, Response, NextFunction } from 'express';
+import { MessageService } from '../services/MessageService.js';
 
 export const index = (req: Request, res: Response, next: NextFunction) => {
   try {
