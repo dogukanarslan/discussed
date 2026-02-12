2ab6df12b98bd8baa2f4a053cda2d1d43b8f05c7
Dogukan Arslan <dogukan2arslan@hotmail.com>
2026-02-13 00:17:26 +0300

    Add authorization header

--- server.ts
+++ server.ts
@@ -29,7 +29,7 @@ var allowCrossDomain = function (
   next: NextFunction,
 ) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Methods', '*');
   next();
 };
