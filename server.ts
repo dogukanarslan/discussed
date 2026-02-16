971413f58ed7eb7d3571e68e610d3fc5d7a6182c
Dogukan Arslan <dogukan2arslan@hotmail.com>
2026-02-17 01:14:49 +0300

    Fix typo

--- server.ts
+++ server.ts
@@ -52,5 +52,5 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => {
-  console.log('server is runnning on port ', PORT);
+  console.log('server is running on port ', PORT);
 });
